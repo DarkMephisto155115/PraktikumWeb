46a09c1d3101f88d7320059fcc99128f694d9a55
Andika Salsabilah <andika.as995@gmail.com>
2024-10-24 20:41:50 +0700

    awifhoiuschgjadrhydvscacgj

---
+++ codelab/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+namespace Controller;
+
+class Controller
+{
+    var $controllerName;
+    var $controllerMethod;
+
+    public function getControllerAttributes(){
+        return["ControllerName" => $this->controllerName,
+    "ControllerMethod" => $this->controllerMethod,
+    ];
+    }
+}
+
+
+
+?>
